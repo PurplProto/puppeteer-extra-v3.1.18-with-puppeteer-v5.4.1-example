3dfaff3300037e3ff175207183adda87754cd3ce
PairedPrototype <16809256+PairedPrototype@users.noreply.github.com>
2021-06-03 03:11:38 +0100

    Created nearly minimal example of type casting Puppeteer types when using the Puppeteer-Extra package

---
+++ README.md
@@ -0,0 +1,3 @@
+# Puppeteer-Extra (v3.1.18) with Puppeteer (v5.4.1) example
+
+Just an example of how I cast the types between the Pupeteer and Puppeteer-Extra packages.
---
+++ index.ts
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+import puppeteerTypes from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import { URL } from 'url';
+
+async function processUrl(browser: puppeteerTypes.Browser, url: URL) {
+  browser.on('disconnected', () => {
+    console.error('Browser no longer available! Did it crash?', 'error', 1);
+    process.exit(1);
+  });
+
+  async function handlePageClose(page: puppeteerTypes.Page) {
+    console.log(`Closing page!`);
+    await page.close();
+    if (!page.isClosed()) {
+      console.log('Page did not close!', 'error');
+    }
+  }
+
+  console.log(`processing URL: ${url.href}`);
+
+  const page = await browser.newPage();
+
+  let response: puppeteerTypes.Response | null;
+  try {
+    response = await page.goto(url.href);
+  } catch (error) {
+    console.log(`Failed to navigate to page: ${url.href}`, 'error');
+    handlePageClose(page);
+    process.exit(1);
+  }
+
+  if (response && !response.ok() && response.status() > 400 && response.status() !== 503) {
+    console.log(
+      `Got a non-success status for page: ${url.href} of "${response.status()}: ${response.statusText()}"`
+    );
+    handlePageClose(page);
+    process.exit(1);
+  }
+
+  const content = await page.content();
+  await handlePageClose(page);
+  // Do stuff with content
+  process.exit(0);
+}
+
+puppeteer.use(AdblockerPlugin()).use(StealthPlugin());
+puppeteer.launch({ headless: false })
+  .then(browser => {
+    processUrl(browser, new URL('https://google.com'));
+  })
+  .catch(e => {
+    console.error('something got borked!\n\n', e);
+    process.exit(1);
+  });
